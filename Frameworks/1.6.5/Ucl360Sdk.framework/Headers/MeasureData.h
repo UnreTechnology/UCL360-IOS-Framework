3cd787873116f59c0fbf327814afbe0a5dcdaaaa
unre-mac <cunhuan.liu@unretek.com>
2021-07-23 18:18:24 +0800

    修复businessConfig转换报错

---
+++ Frameworks/1.6.5/Ucl360Sdk.framework/Headers/MeasureData.h
@@ -0,0 +1,59 @@
+//
+//  Header.h
+//  Ucl360Sdk
+//
+//  Created by unre on 2021/6/10.
+//
+
+#ifndef MeasureData_h
+#define MeasureData_h
+
+NS_ASSUME_NONNULL_BEGIN
+
+
+@interface MeasureValue : NSObject
+
+@property NSArray<NSString*>* value;
+@property NSString* result;
+@property NSString* ruleNo;
+@property NSString* designValues;
+@property NSArray<NSArray<NSNumber*>*>* coordinate;
+@property NSArray<NSNumber*>* cadCoordinate;
+@property NSArray<NSArray<NSNumber*>*>* rules;
+@property NSArray<NSNumber*>* contourCoordinate;
+@property NSString* id;
+
+@end
+
+@interface Measurement : NSObject
+
+@property NSString* id;
+@property NSArray<MeasureValue*>* points;
+
+@end
+
+@interface MeasureObject : NSObject
+
+@property NSString* id;
+@property NSArray<Measurement*>* measurements;
+@property NSString* walls;
+@property NSString* direction;
+
+@end
+
+
+@interface MeasureData: NSObject
+
+@property MeasureObject* room;
+@property NSArray<MeasureObject*>* ceiling;
+@property MeasureObject* floor;
+@property NSArray<MeasureObject*>* walls;
+@property NSArray<MeasureObject*>* multiwalls;
+@property NSArray<MeasureObject*>* openings;
+
+@end
+
+
+NS_ASSUME_NONNULL_END
+
+#endif /* MeasureData_h */
