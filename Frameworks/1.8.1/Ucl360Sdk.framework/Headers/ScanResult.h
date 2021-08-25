7e843648187e2badedf1d9565bfea95c7f300439
unre-mac <cunhuan.liu@unretek.com>
2021-08-25 18:58:07 +0800

    新增站点

---
+++ Frameworks/1.8.1/Ucl360Sdk.framework/Headers/ScanResult.h
@@ -0,0 +1,38 @@
+//
+//  NSObject+ScanResult.h
+//  unrelib
+//
+//  Created by unre on 2021/5/26.
+//
+
+#import <Foundation/Foundation.h>
+#import "ContourFile.h"
+#import "MeasureData.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ScanResult: NSObject
+
+@property NSString* version;
+@property NSString* unreTaskId;
+@property NSString* ispass;
+
+@property NSString* deviceId;
+@property NSString* deviceVersion;
+
+@property MeasureData* data;
+
+@property NSArray<ContourFile*>* files;
+
+@property double credibility;
+@property NSArray<NSNumber*>* matrix;
+@property NSDictionary* mapping;
+@property NSDictionary* drawing_outline;
+@property NSDictionary* scan_roomContour;
+@property NSDictionary* bestForMatch;
+@property NSArray* recommendedScanStation;
+@property NSString* mergeId;
+
+@end
+
+NS_ASSUME_NONNULL_END
