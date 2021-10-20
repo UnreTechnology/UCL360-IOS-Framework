df323b25489b0d0630aea10183d473b3f486d58d
unre-mac <cunhuan.liu@unretek.com>
2021-10-20 14:38:07 +0800

    1 支持删除墙面与顶板 2 扫描错误添加imu异常 3 添加 oldIds参数

---
+++ Frameworks/1.9.9/Ucl360Sdk.framework/Headers/UnreDevice.h
@@ -0,0 +1,33 @@
+//
+//  UnreDevice.h
+//  unrelib
+//
+//  Created by unre on 2021/6/1.
+//
+
+#import <Foundation/Foundation.h>
+#import <OnScanListener.h>
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UnreDevice : NSObject
+
+-(void) interrupt;
+//-(BOOL) startScan:(NSString *)unreMeasurementRegion :(NSString *)ruleId :(NSString *)drawId :(NSString*) extras :(NSString*) standards;
+-(ScanError) startScan:(NSString *)unreMeasurementRegion :(NSString *)ruleId :(NSString *)drawId :(NSString*) extras :(NSString*) standards;
+
+-(ScanError) startScan:(NSString *)unreMeasurementRegion :(NSString *)ruleId :(NSString *)drawId :(NSString*) extras :(NSString*)  standards:(NSArray<NSNumber*>*)coordinate :(NSString*) oldIds;
+
+-(void) registerScanCallback:(id<OnScanListener>) listener;
+
+-(BOOL) isDeviceWorking;
+
+-(void) inMuteMode:(BOOL) enable;
+
+-(NSNumber*) getTaskProgress:(NSString*) taskId;
+-(NSString*) getTaskState:(NSString*) taskId;
+
+@end
+
+NS_ASSUME_NONNULL_END
